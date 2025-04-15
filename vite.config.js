146dd9f72d03cd7c9ed8bcf7264b1a8d88fa6b86
Renaud <64860831+SuperSonicSites@users.noreply.github.com>
2025-04-15 13:57:52 -0400

    Change bundling

--- vite.config.js
+++ vite.config.js
@@ -2,15 +2,15 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   build: {
-    outDir: 'dist', // Use a separate folder for built files
+    outDir: 'dist', // Remember: use a separate directory so /public isn't overwritten
     rollupOptions: {
+      inlineDynamicImports: true, // <-- This tells Rollup to bundle everything in one file
       input: {
         hedge: './hedge/script.js',
         mowing: './mowing/script.js',
         mulching: './mulching/script.js'
       },
       output: {
-        // Bundled files will appear in subfolders in /dist
         entryFileNames: '[name]/[name]-calculator.js'
       }
     }
