9888c4191ee8b745a9f18bc36bd63dc49a39839b
Renaud <64860831+SuperSonicSites@users.noreply.github.com>
2025-04-15 13:30:32 -0400

    update vite config

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,20 @@
-export default {
-    build: {
-      outDir: 'public',
-      rollupOptions: {
-        input: {
-          hedge: './hedge/hedge-calculator.js',
-          lawn: './lawn/lawn-calculator.js',
-          mulch: './mulch/mulch-calculator.js',
-        },
-        output: {
-          entryFileNames: '[name]/[name]-calculator.js'
-        }
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  build: {
+    outDir: 'public',
+    rollupOptions: {
+      input: {
+        hedge: './hedge/script.js',
+        lawn: './lawn/script.js',
+        mulch: './mulch/script.js'
+      },
+      output: {
+        // This renames your output files.
+        // For example, the `hedge` entry will output to `public/hedge/hedge-calculator.js`
+        // even though the source was named "script.js".
+        entryFileNames: '[name]/[name]-calculator.js'
       }
     }
   }
+});
